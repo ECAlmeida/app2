488fb5bccf1ccf39bcf7c58122d83648e5091158
Eder do Carmo Almeida <ederdoalmeida@hotmail.com>
2020-09-27 00:02:03 -0300

    Criacao de Servicos

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.services'
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {}
+  
+
+  ngOnInit(): void {
+    let ofertas: OfertasService = new OfertasService()
+    console.log(ofertas.getOfertas())
+  }
+
+}
