93e175a1a142d9dee88bb07eaec611e6f91c6bf7
Eder do Carmo Almeida <ederdoalmeida@hotmail.com>
2020-09-27 22:56:50 -0300

    Servico OfertaService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.services'
+import { Oferta } from '../shared/oferta.model'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {}
+  public ofertas: Oferta[]
+
+  constructor(private ofertasService: OfertasService) {
+    
+  }
   
 
   ngOnInit(): void {
-    let ofertas: OfertasService = new OfertasService()
-    console.log(ofertas.getOfertas())
+    this.ofertas = this.ofertasService.getOfertas()
+ 
   }
 
 }
